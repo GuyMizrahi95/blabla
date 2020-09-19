03b4210cce89758e188528c9f154a59af116fb70
GuyMizrahi95 <71549229+GuyMizrahi95@users.noreply.github.com>
2020-09-19 19:21:16 +0300

    Create ExistenceException.java

---
+++ ExistenceException.java
@@ -0,0 +1,5 @@
+public class ExistenceException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public ExistenceException(String errorMessage) {super(errorMessage);}
+}
